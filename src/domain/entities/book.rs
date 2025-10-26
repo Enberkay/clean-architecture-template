7ecfe1da29c16636a6c61a09f38f86a4faee45a2
Celine <enberkaych@gmail.com>
2025-10-26 21:38:36 +0700

    feat: Add book entity

--- src/domain/entities/book.rs
+++ src/domain/entities/book.rs
@@ -2,12 +2,11 @@ use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone)]
 pub struct BookEntity {
-    pub isbn: i32,
+    pub isbn: String,
     pub title: String,
     pub author: String,
     pub synopsis: String,
-    pub price: f32,
-    pub cover_image_url: String,
+    pub price: f64,
     pub is_active: bool,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
