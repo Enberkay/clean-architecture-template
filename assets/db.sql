b561a4f9ae41d0032435d6d246e3025fe6f84990
Celine <enberkaych@gmail.com>
2025-10-27 07:58:09 +0700

    feat(CategoryEntity): Add rename, update_description methods

--- assets/db.sql
+++ assets/db.sql
@@ -82,7 +82,8 @@ CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
     description TEXT,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE book_categories (
--- src/domain/entities/category.rs
+++ src/domain/entities/category.rs
@@ -6,4 +6,17 @@ pub struct CategoryEntity {
     pub name: String,
     pub description: Option<String>,
     pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl CategoryEntity {
+    pub fn rename(&mut self, new_name: String) {
+        self.name = new_name;
+        self.updated_at = Utc::now();
+    }
+
+    pub fn update_description(&mut self, desc: Option<String>) {
+        self.description = desc;
+        self.updated_at = Utc::now();
+    }
 }
