0a47df115c1a85699a69385821de6c333bc90f08
Celine <enberkaych@gmail.com>
2025-10-27 07:51:00 +0700

    feat(BookEntity): Add (behavior) activate, deactivate, update_price, update_info, is_available methods

--- src/domain/entities/book.rs
+++ src/domain/entities/book.rs
@@ -12,3 +12,31 @@ pub struct BookEntity {
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
 }
+
+impl BookEntity {
+    pub fn activate(&mut self) {
+        self.is_active = true;
+        self.updated_at = Utc::now();
+    }
+
+    pub fn deactivate(&mut self) {
+        self.is_active = false;
+        self.updated_at = Utc::now();
+    }
+
+    pub fn update_price(&mut self, new_price: Money) {
+        self.price = new_price;
+        self.updated_at = Utc::now();
+    }
+
+    pub fn update_info(&mut self, title: String, author: Option<String>, synopsis: Option<String>) {
+        self.title = title;
+        self.author = author;
+        self.synopsis = synopsis;
+        self.updated_at = Utc::now();
+    }
+
+    pub fn is_available(&self) -> bool {
+        self.is_active
+    }
+}
