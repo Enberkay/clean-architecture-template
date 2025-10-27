03c42cd13fc28ab93bda01f1276b10024b37d80c
Celine <enberkaych@gmail.com>
2025-10-27 07:54:06 +0700

    feat(BranchEntity): Add update_info method

--- src/domain/entities/branch.rs
+++ src/domain/entities/branch.rs
@@ -8,3 +8,11 @@ pub struct BranchEntity {
     pub phone: Option<String>,
     pub created_at: DateTime<Utc>,
 }
+
+impl BranchEntity {
+    pub fn update_info(&mut self, name: String, address: Option<String>, phone: Option<String>) {
+        self.name = name;
+        self.address = address;
+        self.phone = phone;
+    }
+}
